5faa89fa0505fc18bf4b4df8cdee7a3f118bd287
TML <lmike92@gmail.com>
2022-04-18 06:19:04 -0400

    sorting out the logic for climate data retrieval

--- 02-code-scripts/retrieve_climate_data.R
+++ 02-code-scripts/retrieve_climate_data.R
@@ -1,2 +1,11 @@
 # build a script to use this service intelligently
 # https://daymet.ornl.gov/web_services
+
+# round all lat long to 1 decimal places http://wiki.gis.com/wiki/index.php/Decimal_degrees
+round_to <- 1
+
+  merged_morel_data %>% 
+  mutate(short_lat = round(latitude, round_to), short_long = round(longitude, round_to)) %>% 
+  group_by(short_lat, short_long) %>% 
+  summarise(n = n()) %>% 
+    arrange(-n)
